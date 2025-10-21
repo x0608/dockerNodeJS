f01fc7921c93ae4b162ef3dc358884c1bed71ed1
ray <x06082010@gmail.com>
2025-10-21 16:21:38 +0800

    123

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ db.connect(err => {
 
 // 查詢所有 customers
 app.get('/customers', (req, res) => {
-  db.query('SELECT * FROM customers limit 2', (err, results) => {
+  db.query('SELECT * FROM customers limit 5', (err, results) => {
     if (err) return res.status(500).json({ error: err.message });
     res.json(results);
   });
